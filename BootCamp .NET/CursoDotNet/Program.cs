25b54113596788420534a91fd98c1d21dae6c4b5
bakaykun <Kelvin.patrick.174@gmail.com>
2022-02-17 10:39:29 -0300

    segunda aula do curso

--- BootCamp .NET/CursoDotNet/Program.cs
+++ BootCamp .NET/CursoDotNet/Program.cs
@@ -6,7 +6,13 @@ namespace CursoDotNet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numeroDeVezes = 10;
+
+            for (int i = 0; i < numeroDeVezes; i++)
+            {
+                Console.WriteLine($"Vez: {i}");
+            }
+
         }
     }
 }

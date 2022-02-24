37156a1f07e1246569473c729fe1fa25c6bc9725
bakaykun <Kelvin.patrick.174@gmail.com>
2022-02-23 21:21:02 -0300

    tipos de dados em c#

---
+++ BootCamp .NET/TipoDeValores/Pessoa.cs
@@ -0,0 +1,7 @@
+namespace TipoDeValores {
+    public class Pessoa{
+        public int idade { get; set; }
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+    }
+}
---
+++ BootCamp .NET/TipoDeValores/Program.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+
+namespace TipoDeValores {
+
+    class Program {
+        static int Adiconar20(int x){
+        return x+20;
+    }
+
+    static void TrocarNome(Pessoa p1, string nomeNovo) {
+        p1.Nome = nomeNovo;
+    }
+    public void TipoValor(){
+        /* Tipo Valor
+            int y = 3;
+            ///Adiconar20(y); Sem retorno, somente cópia
+            // com o retorno da cópia 
+            y = Adiconar20(y);
+
+            WriteLine($"o valor é {y}"); */
+    }
+        static void Main(string[] args) {
+
+            Pessoa p1 = new Pessoa();
+            p1.Nome = "Kelvin";
+            p1.idade = 23;
+            p1.Documento = "7895965635";
+
+            TrocarNome(p1, "Marcósias");
+            Pessoa p2 = p1;
+
+            WriteLine($@"O nome é {p1.Nome}
+            e {p2.Nome}");
+
+        }
+    }
+
+}

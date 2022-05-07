f621fd2315dcf190e8ac8e9d29eec4156ade7afe
bakaykun <Kelvin.patrick.174@gmail.com>
2022-05-07 19:43:59 -0300

    Atualizando não sei o que

--- Códigos em C e C++/Testes simples/aula.c
+++ Códigos em C e C++/Testes simples/aula.c
@@ -2,38 +2,12 @@
 
 #include <stdlib.h>
 
-int main()
+int main(){
 
-{
+  int i = 2;
 
-  float a;
-
-  float *p, *q;
-
-  a = 3.14;
-
-  printf("%f\n", a);
-
-  p = &a;
-
-  *p = 2.718;
-
-  printf("%f\n", a);
-
-  a = 5;
-
-  printf("%f\n", *p);
-
-  p = NULL;
-
-  p = (float *) malloc(sizeof(float));
-
-  *p = 20;
-
-  q = p;
-
-  printf("%f\n", *p);
-
-  printf("%f\n", *q);
+  while ( i < 5){
+    printf("oi");
+  }
 
 }

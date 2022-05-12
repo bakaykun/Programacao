f4af975ef96c2e9c40a8ec714859d8a4d3a2a2e4
bakaykun <Kelvin.patrick.174@gmail.com>
2022-05-11 21:53:12 -0300

    Atualização do Programa em JS

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -10,8 +10,18 @@ let listElement = document.getElementById('lista');
 
 function addItem(){
 	let getText = inputElement.value;
-	let textItem = document.createElement(getText);
+	let textItem = document.createTextNode(getText);
 	let liElement = document.createElement('li');
+	let aElement = document.createElement('a');
 
-	listElement.appendChild
+	listElement.appendChild(liElement);
+	liElement.appendChild(textItem);
+}
+
+/* ao clicar no botão a função addItem() é chamada para criar um novo item em tela,
+em seguida limpamos o campo para digitarmos um novo item a ser inserido*/
+
+buttonElement.onclick = function () {
+	addItem();
+	inputElement.value = '';
 }

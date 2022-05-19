0f49f0a2480c633cd504288cccde93a3309ba18d
bakaykun <Kelvin.patrick.174@gmail.com>
2022-05-18 21:44:40 -0300

    Banco de dados Humilde

---
+++ SQL/TabelaDoCapituloSobreSelect.sql
@@ -0,0 +1,16 @@
+use estudos;
+select * from aluno;
+
+insert into aluno (nome_aluno, cidade_aluno, media_aluno)
+values ('Carlos', 'Curitiba', 3.4);
+
+insert into aluno (nome_aluno, cidade_aluno, media_aluno)
+values ('Carla', 'Paranavai', 7.4);
+
+insert into aluno (nome_aluno, cidade_aluno, media_aluno)
+values ('Joana', 'Londrina', 9.4);
+
+insert into aluno (nome_aluno, cidade_aluno, media_aluno)
+values ('Pedro', 'Terra Boa', 6.3);
+
+select * from aluno;

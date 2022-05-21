e9556732699481a98c7944cdc90a38f2da00e085
bakaykun <Kelvin.patrick.174@gmail.com>
2022-05-21 17:55:25 -0300

    Módulo sobre arquivos e diretórios

---
+++ BootCamp .NET/Diretorio/Program.cs
@@ -0,0 +1,79 @@
+﻿//Chamada das funções
+criarDiretoriosDoMundo();
+criarArquivo();
+
+//Movendo o arquivo
+var origem = Path.Combine(Environment.CurrentDirectory, "globo", "América do Sul", "Brasil.ext");
+var destino = Path.Combine(Environment.CurrentDirectory, "globo", "América do Sul", "Brasil", "Brasil.ext");
+
+moverArquivo(origem, destino);
+
+static void moverArquivo(string pathOrigem, string pathDestino){
+
+    if(!File.Exists(pathOrigem)) {
+        Console.WriteLine("Arquivo de Origem Não Existe");
+        return;
+    }
+
+    if(File.Exists(pathDestino)){
+        Console.WriteLine("Arquivo de destijo já existe");
+        return;
+    }
+    
+    File.Move(pathOrigem, pathDestino);
+
+    
+}
+
+static void criarArquivo(){
+    var path = Path.Combine(Environment.CurrentDirectory, "Brasil.ext");
+
+    if(!File.Exists(path)){
+        using var sw = File.CreateText(path);
+        sw.WriteLine("Populacao: 213.000.123");
+        sw.WriteLine("IDH: 0.95");
+        sw.WriteLine("Dados Atualizados em 21/05/2018");
+    }
+
+}
+
+static void criarDiretoriosDoMundo(){
+    //criando o diretório central
+    var path = Path.Combine(Environment.CurrentDirectory, "globo");
+    
+    if(!Directory.Exists(path)){
+
+        var dirNacao = Directory.CreateDirectory(path);
+        //Criando os sub diretórios 
+        var dirAmNorte = dirNacao.CreateSubdirectory("América do Norte");
+        var dirAmSul = dirNacao.CreateSubdirectory("América do SUl");
+        var dirAmCentral = dirNacao.CreateSubdirectory("América Central");
+
+        //criando o sub diretório do sub diretório
+        //AMérica do norte
+        dirAmNorte.CreateSubdirectory("México");
+        dirAmNorte.CreateSubdirectory("Canada");
+        dirAmNorte.CreateSubdirectory("Alasca");
+
+        //América do Sul
+        dirAmSul.CreateSubdirectory("Brasil");
+        dirAmSul.CreateSubdirectory("Chile");
+        dirAmSul.CreateSubdirectory("Bolivia");
+
+        //América CENTRAL
+        dirAmCentral.CreateSubdirectory("Costa Rica");
+        dirAmCentral.CreateSubdirectory("Panamá");
+        dirAmCentral.CreateSubdirectory("Honduras");
+
+    } else {
+        var newPath = Path.Combine(Environment.CurrentDirectory, "Existia e eu fui criado");
+        var dirExecucao = Directory.CreateDirectory(newPath);
+
+        dirExecucao.CreateSubdirectory("De novo");
+    }
+
+
+}
+
+
+
---
+++ BootCamp .NET/LerDiretorios/Program.cs
@@ -0,0 +1,49 @@
+﻿var path = @"C:\Users\Bakay\Documents\Programação\Programacao\BootCamp .NET\FilesAndStreams";
+//LerDiretorios(path);
+lerArquivos(path);
+
+System.Console.WriteLine("Pressione [ENTER] para continuar");
+System.Console.ReadLine();
+
+static void lerArquivos(string path){
+    
+    var arquivos = Directory.GetFiles(path,"*",SearchOption.AllDirectories);
+
+    foreach(var aqv in arquivos){
+
+        var infoArquivo = new FileInfo(aqv);
+    
+        System.Console.WriteLine($"Nome: {infoArquivo.Name}");
+        System.Console.WriteLine($"Tamanho: {infoArquivo.Length} Kb");
+        System.Console.WriteLine($"Ultimo Acesso: {infoArquivo.LastAccessTime} ");
+        System.Console.WriteLine($"Pasta: {infoArquivo.DirectoryName}");
+        System.Console.WriteLine("");
+        System.Console.WriteLine("");
+
+
+    }
+}
+
+static void LerDiretorios(string path){
+
+    if(Directory.Exists(path)){
+        var diretorios = Directory.GetDirectories(path,"*", SearchOption.AllDirectories);
+        
+        foreach(var dir in diretorios){
+
+            var dirInfo = new DirectoryInfo(dir);
+
+            System.Console.WriteLine($"Nome: {dirInfo.Name}");
+            System.Console.WriteLine($"Raiz: {dirInfo.Root}");
+            if(dirInfo.Parent != null)
+                System.Console.WriteLine($"Pai: {dirInfo.Parent.Name}");
+            System.Console.WriteLine("========================");
+
+
+        }
+
+    }else {
+        System.Console.WriteLine($"O diretório {path} não existe");
+    }
+
+}

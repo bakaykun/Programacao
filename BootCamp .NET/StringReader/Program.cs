cdde9f953d40f9c032bc65354f1c1dd7ad81e26f
bakaykun <Kelvin.patrick.174@gmail.com>
2022-05-24 23:00:51 -0300

    Aula sobre manipulação de buffer

---
+++ BootCamp .NET/StringReader/Program.cs
@@ -0,0 +1,49 @@
+﻿//blibioteca pra usar a funcionalidade
+using System.Text;
+
+var stringBuilder = new StringBuilder();
+
+//criando linha dentro do objeto
+
+stringBuilder.AppendLine("A cor da Pedrinho Hoje tem campeonato");
+stringBuilder.AppendLine("Asfgdsfgsdfgsdato");
+stringBuilder.AppendLine("A fgdfgsdfgsdfgsdfgsdampeonato");
+stringBuilder.AppendLine("A cor dfsgsdfgdsfgdsfds ftgdsfg sampeonato");
+
+//fazendo a leitura do objeto
+
+using var stringRader = new StringReader(stringBuilder.ToString());
+
+/*Forma grande
+/bufferizando o objeto
+var buffer = new char[10];
+var posicao = stringRader.Read(buffer);
+
+/recompondo o objeto bufferizado da forma zuada
+Console.WriteLine($"{string.Join(" ", buffer)}");
+
+Lendo o objeto na mesma linha:
+
+-> Com um texto grande
+
+var buffer = new char[10];
+var tamanho = 0;
+do {
+    //declarando ele de novo para garantir que ele tá limpo
+    buffer = new char[10];
+    tamanho = stringRader.Read(buffer);
+    Console.Write($"{string.Join("", buffer)}");
+
+}while(tamanho >= buffer.Length);*/
+
+//Forma Enxuta > coisa pequena
+
+do {
+    Console.WriteLine(stringRader.ReadLine());
+} while (stringRader.Peek() >= 0);
+
+
+Console.Write("\n");
+Console.Write("");
+Console.WriteLine("Tecle [ENTER] para continuar");
+Console.ReadKey();
---
+++ BootCamp .NET/StringWriter/Program.cs
@@ -0,0 +1,52 @@
+﻿var textReader = "Acorda Pedrinho que hoje tem campeonato.\n\n" + 
+                "Hoje tem campeonato.\n\n" + "Vem dançar comigo";
+
+System.Console.WriteLine($"Texto Original: " + textReader);
+
+string linha, paragrafo = null;
+var sr = new StringReader(textReader);
+
+while(true){
+
+    linha = sr.ReadLine();
+
+    if ( linha != null) {
+        paragrafo += linha + " ";
+    }
+    else {
+        paragrafo += "\n";
+        break;
+    }
+}
+
+System.Console.WriteLine("TEXTO MODIFICADO: " + paragrafo);
+
+//Lendo o texto na forma decimal
+
+int caractereLido;
+char caractereConvertido;
+
+var sw = new StringWriter();
+sr = new StringReader(paragrafo);
+
+while(true) {
+
+    caractereLido = sr.Read(); 
+    if(caractereLido == -1) break;
+
+    caractereConvertido = Convert.ToChar(caractereLido);
+
+    if(caractereConvertido == '.'){
+        sw.Write("\n\n");
+
+        sr.Read();
+        sr.Read();
+
+    }else {
+        sw.Write(caractereConvertido);
+    }
+
+    
+}
+
+System.Console.WriteLine($"TEXTO COM TABELA ASCII:  {sw.ToString()}");

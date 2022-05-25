968c130d3e54df3b4de6591ea9fd9c26f074220c
bakaykun <Kelvin.patrick.174@gmail.com>
2022-05-25 17:32:12 -0300

    Trabalahndo com arquivos csv + commit de algumas atualizações de diretórios

---
+++ BootCamp .NET/csvStream/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+
+var path = Path.Combine(Environment.CurrentDirectory, "pastaDoArquivo", "arquivoTeste.csv");
+
+if (File.Exists(path)){
+    using var sr = new StreamReader(path);
+    var cabecalho = sr.ReadLine()?.Split(',');
+
+    while(true){
+
+        var registro = sr.ReadLine()?.Split(',');
+        if(registro == null) break;
+
+        if(cabecalho.Length != registro.Length){
+            
+            WriteLine("Arquivo fora do padrão !");
+            break;
+        }
+
+        for (int i = 0; i < registro.Length; i++){
+
+            WriteLine($"{cabecalho?[i]}: {registro[i]}");
+
+        }
+
+        WriteLine("-----------");
+    }
+} else {
+    WriteLine($"o {path} não existe !");
+}
+
+WriteLine("Pressione {ENTER} para finalizar");
+ReadLine();
+

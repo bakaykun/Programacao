5cbb07e2cb0a73d24e3edf0a6469da4e94606495
bakaykun <Kelvin.patrick.174@gmail.com>
2022-06-23 18:20:02 -0300

    Aula sobre eventos

--- BootCamp .NET/Delegates/Program.cs
+++ BootCamp .NET/Delegates/Program.cs
@@ -1,9 +1,19 @@
 ﻿using Delegates.models;
 using static System.Console;
 
+/* Delegates apenas
 Operacao op = new Operacao(Calculadora.Somar);
 //com multicast
 op+=Calculadora.Subtrair;
 
 op.Invoke(10,20);
 public delegate void Operacao(int x, int y);
+
+*/
+
+//Usando Eventos
+
+Matematica operacao = new Matematica(35,40);
+operacao.Somar();
+
+
--- BootCamp .NET/Delegates/models/Calculadora.cs
+++ BootCamp .NET/Delegates/models/Calculadora.cs
@@ -2,13 +2,23 @@ namespace Delegates.models
 {
     public class Calculadora
     {
+        //para declarar um evento, tem que declarar o delegate
+        public delegate void DelegateCualculadora();
+
+        public static event DelegateCualculadora EventoCalculadora;
+
         public static void Somar(int x, int y){
-            System.Console.WriteLine($"Adição: {x +y}");
+            if( EventoCalculadora != null){
+                System.Console.WriteLine($"Adição: {x +y}");
+                EventoCalculadora();
+            } else {
+                System.Console.WriteLine("Nenhum inscrito !");
+            }
 
         }
-
         public static void Subtrair(int x, int y){
             System.Console.WriteLine($"Subtração: {x - y}");
         }
+
     }
 }

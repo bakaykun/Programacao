32b5abd97d1eb930951fe75a2d8476a517dd6e20
bakaykun <Kelvin.patrick.174@gmail.com>
2023-02-22 16:38:00 -0300

    Progrmas Exemplos

--- C# + .Net/OitavaAula/OitavaAula/Program.cs
+++ C# + .Net/OitavaAula/OitavaAula/Program.cs
@@ -1,22 +1,74 @@
 ﻿using static System.Console;
 
-namespace OitavaAula {
-    struct Point {
+namespace OitavaAula
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+    struct Point
+    {
         public int X;
         public int Y;
 
-        public override string ToString(){
-            return "(" +    X +")" + "(" +  Y + ")";
+        public override string ToString()
+        {
+            return "(" + X + ")" + "(" + Y + ")";
         }
     }
-    class Program {
-        static void Main(){
-            Point p = new Point();
 
-            p.X = 3;
-            p.Y = 96;
+    static void ExempleProblemOne()
+    {
 
-            Write(" " + p);
+        WriteLine("A altura de quantas pessoas quer medir? ");
+        int qtd = int.Parse(ReadLine());
+
+        double[] vetor = new double[qtd];
+
+        for (int i = 0; i < qtd; i++)
+        {
+            vetor[i] = double.Parse(ReadLine());
+        }
+
+        double soma = 0.0;
+        for (int i = 0; i < qtd; i++)
+        {
+            soma += vetor[i];
+        }
+
+        double mediaAltura = soma / qtd;
+
+        Write("A média das alturas é: " + mediaAltura.ToString("f2"));
+    }
+    class Program
+    {
+        static void Main()
+        {
+            WriteLine("How many products you want calculate");
+            int qtd = int.Parse(ReadLine());
+
+            Product[] p = new Product[qtd];
+
+            for (int i = 0; i < qtd; i++)
+            {
+                string name = ReadLine();
+                double price = double.Parse(ReadLine());
+                p[i] = new Product{Name = name, Price = price};
+            }
+
+            double sum = 0.0;
+
+            for (int i = 0; i < qtd; i++)
+            {
+                sum += p[i].Price;
+            }
+
+            double avarage = sum / qtd;
+
+            WriteLine("Average Price = " + avarage.ToString("f2"));
+
+
         }
     }
 }

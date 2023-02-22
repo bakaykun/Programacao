db14002741bd9ced765a864dce44deb8be51d301
bakaykun <Kelvin.patrick.174@gmail.com>
2023-02-21 22:47:22 -0300

    nada

--- BootCamp .NET/GetSet/Program.cs
+++ BootCamp .NET/GetSet/Program.cs
@@ -1,12 +1,18 @@
 ﻿using GetSet.model;
 
-Data data = new Data();
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Data data = new Data();
 
-Console.WriteLine("Ditete um dia");
-data.Dia = int.Parse(Console.ReadLine());
-Console.WriteLine("Ditete um mes");
-data.Mes = int.Parse(Console.ReadLine());
-Console.WriteLine("Ditete um ano");
-data.Ano = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ditete um dia");
+        data.Dia = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ditete um mes");
+        data.Mes = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ditete um ano");
+        data.Ano = int.Parse(Console.ReadLine());
 
-data.MostrarData();
+        data.MostrarData();
+    }
+}

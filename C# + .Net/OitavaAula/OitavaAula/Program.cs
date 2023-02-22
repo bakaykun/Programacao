0dd09312f6e3774fcf6e8bd095c8cf621433e3dd
bakaykun <Kelvin.patrick.174@gmail.com>
2023-02-21 22:43:48 -0300

    Agora sim a seção 6

---
+++ C# + .Net/OitavaAula/OitavaAula/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+namespace OitavaAula {
+    struct Point {
+        public int X;
+        public int Y;
+
+        public override string ToString(){
+            return "(" +    X +")" + "(" +  Y + ")";
+        }
+    }
+    class Program {
+        static void Main(){
+            Point p = new Point();
+
+            p.X = 3;
+            p.Y = 96;
+
+            Write(" " + p);
+        }
+    }
+}

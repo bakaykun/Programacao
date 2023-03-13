91966802c0c50e4ab456c709704328e9f082b6fa
bakaykun <Kelvin.patrick.174@gmail.com>
2023-03-13 19:32:45 -0300

    Código da Aula sobre Herança

---
+++ C#/AulaOnze/Heranca/Herenca/Entities/BussinesAccount.cs
@@ -0,0 +1,26 @@
+namespace Herenca.Entities
+{
+    public class BussinesAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BussinesAccount() 
+        {
+
+        }
+
+        public BussinesAccount(int number, string holder, double balance, double loanLimit) 
+            : base (number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if(amount <= LoanLimit){
+                Balance += amount - 10.00;
+            }
+        }
+
+    }
+}
---
+++ C#/AulaOnze/Heranca/Herenca/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+namespace Heranca
+{
+    class Program 
+    {
+        static void Main()
+        {
+
+        }
+    }
+}

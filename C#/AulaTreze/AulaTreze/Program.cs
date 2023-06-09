98ee1d280f970a533d663d92e95973ce923b8c02
bakaykun <Kelvin.patrick.174@gmail.com>
2023-06-09 11:19:13 -0300

    COmmit da aula sobre excessão

---
+++ C#/AulaTreze/AulaTreze/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+namespace AulaTreze
+{
+    class Program 
+    {
+        static void Main()
+        {
+            try 
+            {
+                int n1 = int.Parse(ReadLine());
+                int n2 = int.Parse(ReadLine());
+
+                int sum = n1+n2;
+                WriteLine("\nResultado: " + sum + "\n");
+            }
+            catch (FormatException e) 
+            {
+                WriteLine("\nErro !\n" + e.Message);
+                Write("\n Deseja tentar novamente? [s/n]");
+                string op = ReadLine();
+                if(op == "s" || op == "S")
+                {
+                    Write("\nVocê escolheu voltar e tentar novamente, lembre que só podem numeros inteiros !\n");
+                    Main();
+                }
+                else 
+                {
+                    Write("\nVocê escolheu sair !\n");
+                }
+            }
+        }
+    }
+}
